1fd55e6cc45965a51486bf178282c22204c5267f
Dominik <Dominikg.Licari@gmail.com>
2016-03-21 11:09:24 -0500

    Initial constructor

--- src/DomJ.java
+++ src/DomJ.java
@@ -1,24 +1,40 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 public class DomJ extends JFrame
 {
-        TextArea editor;
-        Button run;
-        Button compile;
-        Button jar;
+        private TextArea editor;
+        private Button run;
+        private Button compile;
+        private Button save;
+        private File workingDirectory;
+        private File currentFile;
+        private File configFile;
+        private Scanner conf;
         public DomJ()
         {
                super("DomJ, the best IDE");
                editor = new TextArea();
+               try
+               {
+                       workingDirectory = new Paths(System.getProperty("user.dir"));
+                       configFile = new File(workindDirectory.getAbsolutePath() + "conf");
+                       conf = new Scanner(configFile);
+                       currentFile = new File(conf.nextLine());
+                       save = new Button(new DomJButtons(0, editor));
+               }
+               catch(FileNotFoundExeption e)
+               {
+                       System.err.println("ERROR: " e);
+               }
+               
         }
-        public DomJ(String fileName)
-        {
-               super("DomJ, the best IDE");
-               editor = new TextArea();
-        }
+        
         public static void main(String[] args)
         {
                 new DomJ();
         }
         
 }
+
+
