7f194a34a27538f4f9661b3e6501598e85897967
Dominik <Dominikg.Licari@gmail.com>
2016-03-21 11:07:44 -0500

    Start of buttons class

---
+++ src/DomJButtons.java
@@ -0,0 +1,15 @@
+public class DomJButtons implements ActionListener
+{
+        public DomJButtons(int selector, TextArea code)
+        {
+                if (selector == 0)
+                {
+                        
+                }
+        }
+        
+        public void actionPerformed(ActionEvent e)
+        {
+                
+        }
+}
